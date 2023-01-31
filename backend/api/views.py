955be0e2cd15c7a26c9e2e3a7f81afd46280b967
luskoeqk <lusy2017@gmail.com>
2023-01-31 10:20:58 +0200

    views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,34 @@
 
 # Create your views here.
 
+import json
 from django.http import JsonResponse
 
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'message:': 'Django Response'})
+
+    print(request.GET)  # urls query params
+    print(request.POST)
+    
+    # take the request and turn it into a py dict
+    body = request.body
+    data = {}
+
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    print(data)
+    # print(data.keys())
+
+    # data['headers'] = request.headers
+    # data['conent_type'] = request.conent_type
+    print(request.headers)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+
+    return JsonResponse(
+        # {'message:': 'Django Response'}
+        data
+    )
--- py_client/index.py
+++ py_client/index.py
@@ -5,8 +5,9 @@ endpointAPI = "http://127.0.0.1:8000/api/"
 
 
 # API -> Method
-get_response = requests.get(endpointAPI)
+get_response = requests.get(endpointAPI, params={'abc': 123}, json={"query": "Hello world!"})
 
-print(get_response.text)
-print(get_response.status_code)
+# print(get_response.text)
+# print(get_response.status_code)
 
+print(get_response.json())
